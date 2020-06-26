482343b996916bb239efb6dc26e87b3ecb29de81
Eightdays1 <kevin.noisternig@web.de>
2020-06-26 12:02:40 +0200

    shunting-yard angefangen

--- TaschenrechnerV1/Controller.cpp
+++ TaschenrechnerV1/Controller.cpp
@@ -1,6 +1,7 @@
 #include "Controller.h"
 #include <iostream>
 #include <vector>
+#include <stack>
 
 namespace calculator
 {
@@ -18,7 +19,8 @@ namespace calculator
 	//Method for initializing
 	void Controller::init()
 	{
-		calculate("2+3");
+
+		calculate("20+3");
 	}
 
 	//Is called by GUI if there is an Input. Input is given as String
@@ -59,6 +61,13 @@ namespace calculator
 
 	//converts global vInput to UPN-Format(Prefix-notation)
 	void Controller::convertvInputToUPN() {
+		std::stack<std::string> input;
+		std::vector<std::string> output;
+
+		for (std::string &token : vInput) {
+			std::string Test;
+			Test = token;
+		}
 
 		//shunting-Yard-Algorithm
 
