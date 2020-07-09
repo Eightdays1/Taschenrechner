a1c6171252581a72f3811d1bcfbb1eb2f8386921
Nico Rahm <nico_rahm@hotmail.de>
2020-07-09 20:01:33 +0200

    weiss auch net

--- TaschenrechnerV2/TaschenrechnerV2.h
+++ TaschenrechnerV2/TaschenrechnerV2.h
@@ -9,8 +9,6 @@ class TaschenrechnerV2 : public QMainWindow
 
 public:
     TaschenrechnerV2(QWidget *parent = Q_NULLPTR);
-    void showInput(std::string pstring);
-    void showResult(std::string pString);
 
 private:
     Ui::TaschenrechnerV2Class ui;
